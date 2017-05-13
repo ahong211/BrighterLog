b9103d4062ef7cec19bd8c9e13903fb96c453328
ahong211 <ahong211@gmail.com>
2017-05-13 14:18:56 -0400

    Added another button to read from .xls files

--- app/src/main/java/com/brighterbrain/brighterlog/MainActivity.java
+++ app/src/main/java/com/brighterbrain/brighterlog/MainActivity.java
@@ -1,26 +1,37 @@
 package com.brighterbrain.brighterlog;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Iterator;
 
 //import jxl.Workbook;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = MainActivity.class.getSimpleName();
     EditText editTextFromUser;
     Button buttonToSaveExcel;
+    Button buttonToReadExcel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,17 +40,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         initializeObjects();
         buttonToSaveExcel.setOnClickListener(this);
+        buttonToReadExcel.setOnClickListener(this);
     }
 
     private void initializeObjects() {
         editTextFromUser = (EditText) findViewById(R.id.editTextFromUser);
         buttonToSaveExcel = (Button) findViewById(R.id.buttonToSaveExcel);
+        buttonToReadExcel = (Button) findViewById(R.id.buttonToReadExcel);
     }
 
     @Override
     public void onClick(View view) {
         if (view == buttonToSaveExcel) {
             saveToExcel();
+        }
+
+        if (view == buttonToReadExcel) {
+            String excelNameEditText = editTextFromUser.getText().toString();
+            readExcelFile(getApplicationContext(), excelNameEditText + ".xls");
         }
     }
 
@@ -77,6 +95,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         try {
             outputStream = new FileOutputStream(file);
             workbook.write(outputStream);
+            Log.d(TAG, MainActivity.this.getFilesDir().getAbsolutePath());
+
             Toast.makeText(this, "Write Successful!", Toast.LENGTH_SHORT).show();
         } catch (IOException e) {
             Toast.makeText(this, "Error Writing to File", Toast.LENGTH_SHORT).show();
@@ -142,5 +162,61 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         sheetOne.setColumnWidth(3, (15 * 500));
         sheetOne.setColumnWidth(4, (15 * 500));
         sheetOne.setColumnWidth(5, (15 * 500));
+    }
+
+
+    private static void readExcelFile(Context context, String filename) {
+
+        if (!isExternalStorageAvailable() || isExternalStorageReadOnly())
+        {
+            Log.e(TAG, "Storage not available or read only");
+            return;
+        }
+
+        try{
+            // Creating Input Stream
+            File file = new File(context.getExternalCacheDir(), filename);
+            FileInputStream myInput = new FileInputStream(file);
+
+            // Create a POIFSFileSystem object
+            POIFSFileSystem myFileSystem = new POIFSFileSystem(myInput);
+
+            // Create a workbook using the File System
+            HSSFWorkbook myWorkBook = new HSSFWorkbook(myFileSystem);
+
+            // Get the first sheet from workbook
+            HSSFSheet mySheet = myWorkBook.getSheetAt(0);
+
+            /** We now need something to iterate through the cells.**/
+            Iterator rowIter = mySheet.rowIterator();
+
+            while(rowIter.hasNext()){
+                HSSFRow myRow = (HSSFRow) rowIter.next();
+                Iterator cellIter = myRow.cellIterator();
+                while(cellIter.hasNext()){
+                    HSSFCell myCell = (HSSFCell) cellIter.next();
+                    Log.d(TAG, "Cell Value: " +  myCell.toString());
+                    Toast.makeText(context, "cell Value: " + myCell.toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }catch (Exception e){e.printStackTrace(); }
+
+        return;
+    }
+
+    public static boolean isExternalStorageReadOnly() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isExternalStorageAvailable() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(extStorageState)) {
+            return true;
+        }
+        return false;
     }
 }
